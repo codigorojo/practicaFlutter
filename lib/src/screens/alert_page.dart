bd5e5b21b25fb25ab3b59c37d6418e60826ed9be
FerPM <46204965+codigorojo@users.noreply.github.com>
2020-12-06 21:27:16 -0300

    ensayando unos alerts

--- lib/src/screens/alert_page.dart
+++ lib/src/screens/alert_page.dart
@@ -5,11 +5,42 @@ class AlertPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Alert')),
+      body: Center(
+          child: RaisedButton(
+        child: Text('HolaQueTalComo'),
+        onPressed: () => _showAlert(context),
+        color: Colors.deepOrange,
+        textColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+        padding: EdgeInsets.all(20.0),
+      )),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.ac_unit),
           onPressed: () {
             Navigator.pop(context);
           }),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('hola'),
+            content: Text('csdkcaewkfbn  ckjcasj ki'),
+            actions: [
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('OK')),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('CANCELAME')),
+            ],
+          );
+        });
   }
 }
